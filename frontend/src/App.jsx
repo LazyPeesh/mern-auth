d24b06739fb4c99bf59da9f548dddb2d8270c79e
LazyPeesh <tungthanh254@gmail.com>
2023-06-12 00:23:11 +0700

    on hold

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
 const App = () => {
   return (
     <>
-      <h1>MERN Auth</h1>
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
+
     </>
   )
 }
